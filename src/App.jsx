2c4f9defc0c86b7db098152a4cfd488ff5670dde
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-18 10:36:57 +0530

    Few Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { ChannelList, Chat } from 'stream-chat-react'
+import { Channel } from 'stream-chat-react'
+import Cookies from 'universal-cookie/es6'
+
+const apiKey = "qgtk9ttyha7j";
+
+const client = StreamChat.getInstance(apiKey)l
 
 const App = () => {
     return (
-        <div>
-            <h1>MedChat</h1>
+        <div className="app__wraper">
+            <Chat client={client} theme="team dark">
+                <ChannelListContainer />
+                <ChannelContainer />
+            </Chat>
         </div>
     )
 }
