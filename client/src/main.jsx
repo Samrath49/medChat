a0774fa333c586f5296c53f296150302ef00c388
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2022-06-03 14:36:52 +0530

    Tailwind init

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ChatProvider } from "./context/ChatContext";
-// import "./index.css";
+import "./index.css";
 
 ReactDOM.render(
   <ChatProvider>
