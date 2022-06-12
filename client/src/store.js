c964a32f8a9c096588c014c181efbe3188c2453b
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2022-06-12 07:56:48 +0530

    Store chages

--- client/src/store.js
+++ client/src/store.js
@@ -25,7 +25,7 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 // creating the store
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: [thunk, appApi.middleware],
 });
 
