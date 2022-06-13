157775d5090e6c34f40d8cda581ebf982c6900f6
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2022-06-13 15:33:13 +0530

    Rooms created

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,41 @@
 import { useSelector } from "react-redux";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import { AppContext } from "../context/appContext";
 
 const Sidebar = () => {
-  const rooms = ["first room", "second room", "third room", "fourth room"];
+  // const rooms = ["first room", "second room", "third room", "fourth room"];
   const user = useSelector((state) => state.user);
-  const { socket } = useContext(AppContext);
+  const {
+    socket,
+    setMembers,
+    members,
+    setCurrentRoom,
+    setRooms,
+    privateMemberMsg,
+    rooms,
+    setPrivateMemverMsg,
+    currentRoom,
+  } = useContext(AppContext);
+
+  useEffect(() => {
+    if (user) {
+      setCurrentRoom("general");
+      getRooms();
+      socket.emit("join-room", "general");
+      socket.emit("new-user");
+    }
+  }, []);
 
   socket.off("new-user").on("new-user", (payload) => {
-    console.log("The payload is here:", payload);
+    // console.log("The payload is here:", payload);
+    setMembers(payload);
   });
+
+  function getRooms() {
+    fetch("http://localhost:1234/rooms")
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }
 
   if (!user) return <></>;
   return (
@@ -27,6 +53,13 @@ const Sidebar = () => {
           ))}
         </ul>
         <h2>Members</h2>
+        <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+          {members.map((member) => (
+            <li key={member._id} className="cursor-pointer">
+              {member.username}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
